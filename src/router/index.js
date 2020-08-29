140143ad85537bdc95d3e9f623a03020018ab0a0
Dayo Oladapo <dayoesq@live.com>
2020-08-29 16:49:55 +0300

    webpackChunkName lazy loading added

--- src/router/index.js
+++ src/router/index.js
@@ -14,17 +14,17 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/views/Login.vue')
+    component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue')
   },
   {
     path: '/register',
     name: 'Register',
-    component: () => import('@/views/Register.vue')
+    component: () => import(/* webpackChunkName: "register" */ '@/views/Register.vue')
   },
   {
     path: '/admin',
     name: 'Admin',
-    component: () => import('@/views/admin/Dashboard.vue'),
+    component: () => import(/* webpackChunkName: "admin" */ '@/views/admin/Dashboard.vue'),
     meta: {
       requiresAuth: true,
       isAdmin: true
@@ -49,33 +49,3 @@ router.beforeEach((to, from, next) => {
 })
 
 export default router
-
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
-
-// Vue.use(VueRouter)
-
-// const routes = [
-//   {
-//     path: '/',
-//     name: 'Home',
-//     component: Home
-//   },
-//   {
-//     path: '/about',
-//     name: 'About',
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-//   }
-// ]
-
-// const router = new VueRouter({
-//   mode: 'history',
-//   base: process.env.BASE_URL,
-//   routes
-// })
-
-// export default router
